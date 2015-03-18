0ff5f30b17fab553ad9bcecae6ed32365ace044f
Tomasz D <trzmiel@gmail.com>
2015-03-18 20:06:44 +0100

    Add resume

--- downloader.py
+++ downloader.py
@@ -8,6 +8,7 @@ Common Crawl 2012 Downloader
 import boto
 import sys
 import os
+import argparse
 from itertools import islice
 import multiprocessing as mp
 
@@ -21,7 +22,9 @@ def get_cc_bucket():
     return bucket
 
 
-def get_segments(bucket):
+def get_segments():
+    bucket = get_cc_bucket()
+
     request = CC_PREFIX + '/valid_segments.txt'
     segments = bucket.get_key(request).get_contents_as_string()
     return segments.split()
@@ -48,29 +51,43 @@ def get_filenames(segment):
     return stats
 
 
-def parse_segment(segment):
-    print >> sys.stderr, "Start processing segment: {}".format(segment)
-    print "{}/segment/{}".format(CC_PREFIX, segment)
-    stats = get_filenames(segment)
+def print_stats(stats):
     for k, v in stats.items():
         print k, len(v)
 
-    if not os.path.exists(segment):
-        os.makedirs(segment)
 
+def feed_queue(stats, data_type, downloaded=[]):
     input_queue = mp.Queue()
 
-    # for i in stats['metadata']:
-        # input_queue.put(i)
-
-    for i in stats['textdata']:
-        input_queue.put(i)
+    for t in data_type:
+        for i in stats[t]:
+            if i.split('/')[-1] not in downloaded:
+                input_queue.put(i)
 
     for i in range(6):
         input_queue.put('DONE')
 
-    processes = [mp.Process(target=download_files, args=(segment, input_queue,)) for i in
-                 range(6)]
+    return input_queue
+
+
+def get_downloaded_files(segment):
+    return set(os.listdir(segment))
+
+
+def parse_segment(segment, data_type):
+    print >> sys.stderr, "Start processing segment: {}".format(segment)
+
+    stats = get_filenames(segment)
+
+    print_stats(stats)
+
+    if not os.path.exists(segment):
+        os.makedirs(segment)
+
+    input_queue = feed_queue(stats, data_type, get_downloaded_files(segment))
+
+    processes = [mp.Process(target=download_files,
+                            args=(segment, input_queue,)) for i in range(6)]
     for p in processes:
         p.start()
 
@@ -85,20 +102,27 @@ def download_files(segment, queue):
         if msg == 'DONE':
             break
         filename = './{}/{}'.format(segment, msg.split('/')[-1])
-        bucket.get_key(msg).get_contents_to_filename(filename)
+        try:
+            bucket.get_key(msg).get_contents_to_filename(filename)
+            # print 'Downloading', msg
+            # sys.stdout.flush()
+
+        except IOError:
+            bucket = get_cc_bucket()
+            bucket.get_key(msg).get_contents_to_filename(filename)
 
 
-def process_segments(buckets, segments):
+def process_segments(segments, data_type):
     for segment in segments:
-        parse_segment(segment)
+        parse_segment(segment, data_type)
     print >> sys.stderr, "Finished."
 
 
 def main():
     """ main """
-    bucket = get_cc_bucket()
-    segments = get_segments(bucket)
-    process_segments(bucket, segments[:1])
+    segments = get_segments()
+    data_type = ['textdata']
+    process_segments(segments[:1], data_type)
     print >> sys.stderr, 'Number of segments:', len(segments)
 
 
