21427736e5c5981b1338d66b1a6782e05e0850ca
Tomasz D <trzmiel@gmail.com>
2015-08-25 06:39:07 +0200

    add download checker

---
+++ download_checker.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python2
+
+import glob
+import boto
+import sys
+from itertools import islice
+
+CC_PREFIX = "common-crawl/parse-output"
+
+
+def get_cc_bucket():
+    conn = boto.connect_s3(anon=True)
+    bucket = conn.get_bucket('aws-publicdatasets')
+    return bucket
+
+
+def get_list(bucket, segment):
+    return bucket.list(prefix="{}/segment/{}/".format(CC_PREFIX, segment),
+                       delimiter="/")
+
+
+def get_cc_filenames(segment):
+    stats = {'arc': [], 'metadata': [], 'textdata': []}
+
+    bucket = get_cc_bucket()
+
+    for key in islice(get_list(bucket, segment), None):
+        name = key.name.encode('utf-8').split('/')[-1]
+        if name.endswith('arc.gz'):
+            stats['arc'].append(key.name.encode('utf-8'))
+        elif name.startswith('metadata'):
+            stats['metadata'].append(key.name.encode('utf-8'))
+        elif name.startswith('textData'):
+            stats['textdata'].append(key.name.encode('utf-8'))
+    return stats
+
+
+def get_downloaded_filenames(segment):
+    return glob.glob("./{}/textData*".format(segment))
+
+
+def main():
+    segment = sys.argv[1]
+    print "SEGMENT:", segment
+    cc = get_cc_filenames(segment)['textdata']
+    fs = get_downloaded_filenames(segment)
+    print "CC:", len(cc)
+    print "DOWNLOADED:", len(fs)
+    if len(cc) == len(fs):
+        print "DOWNLOADED CORRECTLY."
+        with open('{}/{}.DOWNLOADED'.format(segment, segment), 'w') as _file:
+            _file.write(str(len(cc)) + '\n')
+
+
+if __name__ == '__main__':
+    main()
